0e2aa8e49b59d3a32166cf97a032a908e719eb40
Jx123408 <jxjin.work@outlook.com>
2026-03-16 22:06:06 +0900

    feat: Add `config.js` for application configuration.

--- assets/js/config.js
+++ assets/js/config.js
@@ -13,7 +13,7 @@ async function customConfirm(msg) {
 // config.js
 // 複数のAPI URL（使用数上限回避のため分散）
 const GAS_API_URLS = [
-  "https://script.google.com/macros/s/AKfycbwEbMvckbiyMIzDjpTukeUbY3gvrlScEzPEcfeJiMQrXpqACwuawjz_AGGRH5csTGBAKA/exec"
+  "https://script.google.com/macros/s/AKfycbzvIwriQoZbogmBfStzXCp03_d18rOBj_ZOZVi1abl2njybi0JsMa3ynmtaQyIUvevOgw/exec"
   //jxjin.ig.school@gmail.com
   //"https://script.google.com/macros/s/AKfycbxGBy7NvMzEdENKL_SYA9Bvk6oq_Fx7_VW2iey0bGFlDfQmGb_-zVP3Gt2pZzJ20m9B/exec",
   //nzn.engeki5@gmail.com
